80462632639563166bb15e4ed5f874ea6b4fbb2d
Anil Kumar <anilbms75@gmail.com>
2021-05-22 13:20:41 +0200

    Get project info

--- src/components/view-one/view-one.js
+++ src/components/view-one/view-one.js
@@ -1,8 +1,25 @@
 import React from 'react';
 import Text from '@commercetools-uikit/text';
+import { useQuery } from "@apollo/client";
+import { GRAPHQL_TARGETS } from "@commercetools-frontend/constants";
+import GetProjectSettings from "./get-project-settings.graphql";
 import messages from './messages';
 
-const ViewOne = () => <Text.Body intlMessage={messages.title} />;
+
+const ViewOne = () => {
+    const { data } = useQuery(GetProjectSettings, {
+        variables: {
+          target: GRAPHQL_TARGETS.COMMERCETOOLS_PLATFORM,
+          limit: 10,
+          offset: 0,
+        },
+      });
+
+    return (<div>
+        {data && <Text.Body>{data.project.name}</Text.Body>}
+        <Text.Body intlMessage={messages.title} />
+    </div>);
+};
 ViewOne.displayName = 'ViewOne';
 
 export default ViewOne;
